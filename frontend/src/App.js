6645fb81b9cc039dd3880643486d49da10d9078f
Danny <49347714+itz-Malc0m@users.noreply.github.com>
2022-04-06 14:00:23 +0100

    store-app-adnek

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">Adnek</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
